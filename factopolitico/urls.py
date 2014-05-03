61b50279b6843de706611d43e356ad6235680c22
Igor Guerrero <igfgt1@gmail.com>
2014-05-03 17:56:17 -0400

    Fix url cause I'm an idiot

--- factopolitico/urls.py
+++ factopolitico/urls.py
@@ -9,17 +9,17 @@ urlpatterns = patterns('',
     url(r'^$', 'facto.views.home', name='home'),
 
     url(r'^countries/$', 'facto.views.countries', name='countries'),
-    url(r'^countries/(?P<slug>\w+)/$', 'facto.views.get_country', name='get_country'),
-    url(r'^countries/(?P<slug>\w+)/facts/$', 'facto.views.country_fact_list', name='country_fact_list'),
+    url(r'^countries/(?P<slug>[\w-]+)/$', 'facto.views.get_country', name='get_country'),
+    url(r'^countries/(?P<slug>[\w-]+)/facts/$', 'facto.views.country_fact_list', name='country_fact_list'),
 
     url(r'^people/$', 'facto.views.people', name='people'),
-    url(r'^people/(?P<slug>\w+)/$', 'facto.views.get_people', name='get_people'),
-    url(r'^people/(?P<people_slug>\w+)/facts/(?P<fact_slug>\w+)/$', 'facto.views.get_fact', name='get_fact'),
+    url(r'^people/(?P<slug>[\w-]+)/$', 'facto.views.get_people', name='get_people'),
+    url(r'^people/(?P<people_slug>[\w-]+)/facts/(?P<fact_slug>[\w-]+)/$', 'facto.views.get_fact', name='get_fact'),
 
     url(r'^recent_facts/$', 'facto.views.recent_facts', name='recent_facts'),
 
     url(r'^categories/$', 'facto.views.categories', name='categories'),
-    url(r'^categories/(?P<slug>\w+)/$', 'facto.views.get_category', name='get_category'),
+    url(r'^categories/(?P<slug>[\w-]+)/$', 'facto.views.get_category', name='get_category'),
 
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
